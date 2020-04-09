3fe358c12c3ea2a5fa8411fcca7fcf21ea4d7941
Ivan Frolov <Ivan.Frolov@bsc-ideas.com>
2020-04-09 17:30:43 +0300

    Change package name

--- src/main/java/com/microservice/StartApplication.java
+++ src/main/java/com/microservice/StartApplication.java
@@ -1,14 +1,11 @@
-package com.mkyong;
+package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootApplication
 @Controller
